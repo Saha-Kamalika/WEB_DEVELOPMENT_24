49259bc2f65579341f7362803f482a98e36bedc9
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2024-01-24 15:11:37 +0530

    Create index.js

---
+++ JavaScipt/Events/Event writing ways/index.js
@@ -0,0 +1,27 @@
+// HTML events are "things" that happen to HTML elements.
+// When JavaScript is used in HTML pages, JavaScript can "react" on these events.
+
+
+// 👨‍🏫 HTML Events
+// An HTML event can be something the browser does, or something a user does.
+
+// Here are some examples of HTML events:
+
+// An HTML web page has finished loading
+// An HTML input field was changed
+// An HTML button was clicked
+// Often, when events happen, you may want to do something.
+
+// JavaScript lets you execute code when events are detected.
+
+// HTML allows event handler attributes, with JavaScript code, 
+// to be added to HTML elements.
+
+
+
+// section 1️⃣ 4 ways of writing Events in JavaScript
+
+// 1: using inline events alert();
+// 2: By Calling a funcion (We already seen and most common way of writing) 
+// 3: using Inline events (HTML onclick="" property and element.onclick)
+// 4: using Event Listeners (addEventListener and IE's attachEvent)
