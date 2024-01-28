49081760e56d83a37ea4078c05f0fff2fc21c7a3
Kamalika Saha <kamalikasaha01@gmail.com>
2024-01-28 18:55:54 +0530

    Addes files

---
+++ JavaScript/Advanced/JSON/index.js
@@ -0,0 +1,14 @@
+//JSON ->JavaScript Object Notation
+var object = {
+    key_1: "kamalika",
+    key_2: "bally",
+    key_3: 7980169813
+};
+
+var jsonObj= JSON.stringify(object);
+console.log(jsonObj);
+typeof(jsonObj);
+
+var obj= JSON.parse(jsonObj);
+console.log(obj);
+typeof(obj);
