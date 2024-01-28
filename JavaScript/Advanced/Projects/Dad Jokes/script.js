79814a2f3900f872ed666e8ef883eccdebe316e5
Kamalika Saha <kamalikasaha01@gmail.com>
2024-01-29 00:52:52 +0530

    Added files

--- JavaScript/Advanced/Projects/Dad Jokes/script.js
+++ JavaScript/Advanced/Projects/Dad Jokes/script.js
@@ -4,19 +4,34 @@ const jokes= document.querySelector('#joke');
 const jokeBtn= document.querySelector('#jokeBtn');
  
 //through PROMISES
-const generateJokes = () =>{
+// const generateJokes = () =>{
+//     const setHeader ={
+//         headers: {
+//             Accept: "application/json"
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com/', setHeader)
+//     .then((res)=> res.json())
+//     .then((data)=>{
+//         jokes.innerHTML= data.joke;
+//     }).catch((error)=>{
+//         console.log(error);
+//     })
+// }
+//using async await (ES8)
+const generateJokes = async () =>{
+    try{
     const setHeader ={
         headers: {
             Accept: "application/json"
         }
     }
-    fetch('https://icanhazdadjoke.com/', setHeader)
-    .then((res)=> res.json())
-    .then((data)=>{
-        jokes.innerHTML= data.joke;
-    }).catch((error)=>{
-        console.log(error);
-    })
+    const res= await fetch('https://icanhazdadjoke.com/', setHeader)
+    const data= await res.json();
+    jokes.innerHTML= data.joke;
+    }catch(err){
+        console.log(err);
+    }
 }
 
 jokeBtn.addEventListener('click', generateJokes);
