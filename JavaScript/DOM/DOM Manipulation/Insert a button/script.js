6d08c369271da4e4dfa05593ade7ed0075880426
Kamalika Saha <kamalikasaha01@gmail.com>
2024-02-14 12:41:23 +0530

    Added files

---
+++ JavaScript/DOM/DOM Manipulation/Insert a button/script.js
@@ -0,0 +1,6 @@
+let newBtn= document.createElement("button");
+newBtn.innerText= "click me!";
+newBtn.style.color="red";
+newBtn.style.backgroundColor= "yellow";
+
+document.querySelector("body").prepend(newBtn);
