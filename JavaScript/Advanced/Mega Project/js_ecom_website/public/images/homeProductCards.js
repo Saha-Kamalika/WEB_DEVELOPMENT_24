80258f1acdef2a44b91827d576b9212e1cc378bc
Saha-Kamalika <kamalikasaha01@gmail.com>
2024-03-17 23:19:46 +0530

    updated

---
+++ JavaScript/Advanced/Mega Project/js_ecom_website/public/images/homeProductCards.js
@@ -0,0 +1,21 @@
+const prodcutContainer = document.querySelector("#productContainer");
+const productTemplate = document.querySelector("#productTemplate");
+
+export const showProductContainer = (products) =>{
+    if(!products){
+        return false;
+    }
+    products.forEach((currProd)=>{
+        const{brand, category, description, id, image, name, price, stock}=currProd;
+        const productClone = document.importNode(productTemplate.content, true); //for cloning template
+        productClone.querySelector(".category").textContent = category;
+        productClone.querySelector(".productName").textContent = name;
+        productClone.querySelector(".productImage").src = image;
+        productClone.querySelector(".productImage").alt = name;
+        productClone.querySelector(".productDescription").textContent = description;
+        productClone.querySelector(".productStock").textContent = stock;
+        productClone.querySelector(".productPrice").textContent = `₹${price}`;
+        productClone.querySelector(".productActualPrice").textContent = `₹${price*4}`;
+        prodcutContainer.append(productClone);
+    })
+}
