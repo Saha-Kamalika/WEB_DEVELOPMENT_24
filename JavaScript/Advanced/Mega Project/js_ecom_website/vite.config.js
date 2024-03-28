845206d677cf9861d62b24af69f500215b5f17fe
Saha-Kamalika <kamalikasaha01@gmail.com>
2024-03-28 18:59:06 +0530

    done

--- JavaScript/Advanced/Mega Project/js_ecom_website/vite.config.js
+++ JavaScript/Advanced/Mega Project/js_ecom_website/vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
@@ -5,13 +6,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"), // Your main entry point (e.g., home page)
+        main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"), // Additional HTML pages
         contact: resolve(__dirname, "contact.html"),
         products: resolve(__dirname, "products.html"),
         addToCart: resolve(__dirname, "addToCart.html"),
-        registration: resolve(__dirname, "registration.html"),
-        // Add more entry points for other HTML files as needed
+        registration: resolve(__dirname, "registration.html")
       },
     },
   },
